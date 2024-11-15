1fcf48c4fd0c50b4b265de2a168b86099d039744
Nkap pc4 <wansimath123@gmail.com>
2024-11-15 15:11:40 +0100

    changes on hero

--- src/components/card/CardComponent.jsx
+++ src/components/card/CardComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function CardComponent() {
   return (
-    <div className="h-[460px] w-[350px] bg-slate-950 cursor-pointer mb-5 flex flex-col gap-3 rounded overflow-hidden">
+    <div className="h-[460px] w-[350px]  cursor-pointer mb-5 flex flex-col  rounded overflow-hidden">
       <div className="w-[100%] relative h-[300px] overflow-hidden">
         <Image
           src={"/shoes/1.jpg"}
@@ -12,6 +12,11 @@ export default function CardComponent() {
           objectPosition="center"
           alt=""
         />
+      </div>
+      <div>
+        <h1>Air jordan</h1>
+        <p>men's Shoe</p>
+        <h1>$100</h1>
       </div>
     </div>
   );
